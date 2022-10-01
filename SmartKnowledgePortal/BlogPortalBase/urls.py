4f14a90822a3a5b70911ef2c54a08f004c72b0c6
Vivek Vijayan <vivekvijayan3@icloud.com>
2022-10-01 18:29:48 +0530

    Update on blog

--- SmartKnowledgePortal/BlogPortalBase/urls.py
+++ SmartKnowledgePortal/BlogPortalBase/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("LandingBase.urls")),
-    path("login/", include("Authenticator.urls")),
-    path("serco-pmo/", include("PMOPortalBase.urls")),
-    path("blog/", include("BlogPortalBase.urls")),
+    path("", views.BlogPublicPage, name="BlogPublicPage"),
 ]
--- SmartKnowledgePortal/BlogPortalBase/views.py
+++ SmartKnowledgePortal/BlogPortalBase/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def BlogPublicPage(request):
+    return render(request=request, template_name="BlogPublicPage.html", context={})
