0c6db55fe85fc3700a73294ed8a6c208218767d6
vivek-vijayan <vivekvijayan3@icloud.com>
2022-12-02 02:14:53 +0530

    background and login page update

--- SmartKnowledgePortal/PMOPortalBase/urls.py
+++ SmartKnowledgePortal/PMOPortalBase/urls.py
@@ -29,5 +29,5 @@ urlpatterns = [
     path("success/", views.successpage, name="PMOSuccessPage"),
     path("boardingstatus/request-to-deactivate/", views.RequestToDeactivateAccount, name="RequestToDeactivateAccount"),
     path("pmo-admin/", views.PMOAdminPage, name="PMOAdminPage"),
-    path("boardingstatus/pmo-repository/", views.PMORepository, name="PMORepository"),
+    path("pmo-repository/", views.PMORepository, name="PMORepository"),
 ]
